5f0512902403889ca9431d91707ed20b399c1f38
Jan Temme <Jan.Temme@gmail.com>
2015-12-29 15:44:59 +0100

    fixed setup import. Setup now properly working

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-        name='pyrope',
-        version='1.0rc1',
-        packages=['pyrope'],
-        url='https://github.com/Galile0/pyrope',
-        license='GNU GPL v3.0',
-        author='Jan Temme',
-        author_email='jan.temme@gmail.com',
-        install_requires=[
-              'bitstring',
-          ],
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Natural Language :: English',
-            'Programming Language :: Python :: 3 :: Only'
-             ],
-        description='parse and decode replay files from Rocket League'
+    name='pyrope',
+    version='1.0rc1',
+    packages=['pyrope'],
+    url='https://github.com/Galile0/pyrope',
+    license='GNU GPL v3.0',
+    author='Jan Temme',
+    author_email='jan.temme@gmail.com',
+    install_requires=[
+        'bitstring'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    description='parse and decode replay files from Rocket League'
 )
